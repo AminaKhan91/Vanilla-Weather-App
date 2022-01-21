e3bda4b824159071690cb8894e233a271983eb03
AminaKhan91 <96996241+AminaKhan91@users.noreply.github.com>
2022-01-21 10:35:24 +0000

    Added some JS to get real data for some attributes

---
+++ src/app.js
@@ -0,0 +1,26 @@
+function displayTemperature(response) {
+  console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let maxTempElement = document.querySelector("#max-temp");
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+}
+
+let apiKey = "2b57e8c8e53ab345628a7d30def85137";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
